95adfa45d9341344a5cdedd8f3a9ff78be094287
Shivam <shivamch0865@gmail.com>
2026-02-16 19:04:49 +0530

    Minor Changes Done

--- src/app.tsx
+++ src/app.tsx
@@ -11,19 +11,16 @@ import {
   Works,
   StarsCanvas,
 } from "./components";
-import Banner from "./components/banner";
 import Footer from "./components/footer";
 
 // App
 const App = () => {
-  const [hide, setHide] = useState(true);
 
   return (
     <BrowserRouter>
-      <Banner hide={hide} setHide={setHide} />
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar hide={hide} />
+          <Navbar hide={false} />
           <Hero />
         </div>
         <About />
